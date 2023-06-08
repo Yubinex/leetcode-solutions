270433f4377944745b947cdc43011dbc412653f0
Jan Hauck <janhauck99@gmail.com>
2023-06-08 04:06:16 +0200

    Finished two sum problem

---
+++ python/two_sum.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, num in enumerate(nums):
+            tmp: int = target - num
+            if tmp in nums[i+1:]:
+                return [i, nums.index(tmp, i+1)]
+        return False
+
+sol: Solution = Solution()
+
+
+"""
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+"""
+
+
+print(sol.twoSum([2, 7, 11, 15], 9))
+print(sol.twoSum([3, 2, 4], 6))
+print(sol.twoSum([3, 3], 6))
