ee4eaae6f9a56d4196275c4fb531336b367a10cf
Jan Hauck <janhauck99@gmail.com>
2023-06-11 02:37:03 +0200

    Finished two sum problem

---
+++ js/twoSum.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (nums, target) {
+  indexMap = {};
+  for (let i = 0; i < nums.length; ++i) {
+    const tmp = target - nums[i];
+    if (tmp in indexMap && indexMap[tmp] !== i) {
+      return [indexMap[tmp], i];
+    }
+    indexMap[nums[i]] = i;
+  }
+  return undefined;
+};
+
+/*
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
